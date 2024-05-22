5039c39ab562aee67e40146827acc22cd2406122
mariyumsohail <mariyumsohail71@gmail.com>
2024-05-22 07:59:03 +0500

    index.html

---
+++ app.js
@@ -0,0 +1,58 @@
+// var a = 10;
+// document.write("The value of a is : " + a + "\n");
+// var a = ++a; 
+// "\n";
+
+// document.write("The value of ++a is : " + a + "\n" + " Now the value of a is : " + a + "\n");
+// var a = a++;
+// document.write("the value of a++ is : " + a + "\n" )
+// var a = ++a ;
+// document.write("the value of ++a is : " + a + "\n" )
+// var a = --a ; 
+// document.write("the value of --a is : " + a + "\n" + " Now the value of a is 11" )
+// var a = a--; 
+// document.write("the value of a-- is : " + a + "\n" )
+// console.log(a)
+// var a = a--;
+// document.write("the value of a-- is : " + a + "\n" )
+
+// document.write("a is -1, \n b is 0, \n and result is 1.")
+// var name = +prompt("Enter your name ");
+
+// question2
+// var name = +prompt("Enter your name ");
+// alert("Hello there how are you today")
+
+
+
+
+// // question3
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+
+// console.log("a:", a); // Output: a: 1
+// console.log("b:", b); // Output: b: 0
+// console.log("result:", result); // Output: result: 3
+
+
+// // question4
+
+// var sub1 = prompt("Enter the name of the first subject:");
+// var sub2 = prompt("Enter the name of the second subject:");
+// var sub3 = prompt("Enter the name of the third subject:");
+// var totalMarks = 100; 
+// console.log(sub1);
+// console.log(sub2);
+// console.log(sub3);
+
+// var usermarks1 = +prompt("Enter the marks of " + sub1 + " subject:");
+// var usermarks2 = +prompt("Enter the marks of " + sub2 + " subject:");
+// var usermarks3 = +prompt("Enter the marks of " + sub3 + " subject:");
+// console.log(usermarks1);
+// console.log(usermarks1);
+// console.log(usermarks1);
+// // document.write( "marks of english" + usermarks1/totalMarks* 100 )
+// document.write( "marks of english" + usermarks1/totalMarks* 100 )
+
+// // Further calculations and displaying results can be added here
+
